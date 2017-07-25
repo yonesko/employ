ec632e5ab8ba14ecf7c89b06c0159df7961be987
gdanichev <gitgle66>
2017-07-25 15:44:59 +0300

    doubledata; fileupload

--- doubledata/src/main/java/gleb/main/DemoApplication.java
+++ doubledata/src/main/java/gleb/main/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 import java.util.concurrent.Executor;
 
@@ -16,6 +17,8 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+
     }
 
     @Bean
--- doubledata/src/test/java/gleb/DemoApplicationTests.java
+++ doubledata/src/test/java/gleb/DemoApplicationTests.java
@@ -1,16 +1,60 @@
 package gleb;
 
+import gleb.data.model.Task;
+import gleb.main.DemoApplication;
+import gleb.web.UserIdFilter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import javax.servlet.http.Cookie;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = DemoApplication.class)
+@AutoConfigureMockMvc
 public class DemoApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Test
-	public void contextLoads() {
-	}
+    //    @Before
+    void init() {
+        mockMvc = MockMvcBuilders.standaloneSetup().defaultRequest(MockHttpServletRequest::new).build();
+    }
+
+    @Test
+    public void taskAdd() throws Exception {
+        Task task = new Task("http://google.com", "MD5");
+        Cookie cookie = new Cookie(UserIdFilter.USERID, "userid1");
+
+        final int tasknum = 7;
+        for (int i = 0; i < tasknum; i++) {
+            mockMvc.perform(put("/task")
+                    .param("src", task.getSrc())
+                    .param("algo", task.getAlgo())
+                    .cookie(cookie))
+                    .andExpect(status().isOk());
+        }
+
+        mockMvc.perform(get("/task").cookie(cookie)).andExpect(jsonPath("$.length()", "").value(tasknum));
+
+        for (int i = 0; i < tasknum; i++) {
+            mockMvc.perform(get("/task").cookie(cookie)).andExpect(jsonPath("$[%d].src", i).value(task.getSrc()));
+            mockMvc.perform(get("/task").cookie(cookie)).andExpect(jsonPath("$[%d].algo", i).value(task.getAlgo()));
+            mockMvc.perform(get("/task").cookie(cookie)).andExpect(jsonPath("$[%d].status", i).value(task.getStatus().name()));
+            mockMvc.perform(get("/task").cookie(cookie)).andExpect(jsonPath("$[%d].statusPayload", i).isEmpty());
+        }
+
+    }
 
 }
