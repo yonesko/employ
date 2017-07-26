997ed474d135680919b527179ce465ba61e1fba4
gdanichev <gitgle66>
2017-07-26 12:51:06 +0300

    doubledata; add JMX monitoring

--- doubledata/src/main/java/gleb/util/concurrent/TimeAndParallerLimitExecutor.java
+++ doubledata/src/main/java/gleb/util/concurrent/TimeAndParallerLimitExecutor.java
@@ -1,9 +1,12 @@
 package gleb.util.concurrent;
 
 import org.apache.commons.lang3.concurrent.TimedSemaphore;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
 
 import java.util.concurrent.*;
 
+@ManagedResource
 public class TimeAndParallerLimitExecutor implements Executor {
     private Executor executor;
     private TimedSemaphore timedSemaphore;
@@ -32,4 +35,45 @@ public class TimeAndParallerLimitExecutor implements Executor {
     public void execute(Runnable command) {
         queue.add(command);
     }
+
+    @ManagedAttribute
+    public int getQueueSize() {
+        return queue.size();
+    }
+
+    @ManagedAttribute
+    public int getLimit() {
+        return timedSemaphore.getLimit();
+    }
+
+    @ManagedAttribute
+    public int getLastAcquiresPerPeriod() {
+        return timedSemaphore.getLastAcquiresPerPeriod();
+    }
+
+    @ManagedAttribute
+    public int getAcquireCount() {
+        return timedSemaphore.getAcquireCount();
+    }
+
+    @ManagedAttribute
+    public int getAvailablePermits() {
+        return timedSemaphore.getAvailablePermits();
+    }
+
+    @ManagedAttribute
+    public double getAverageCallsPerPeriod() {
+        return timedSemaphore.getAverageCallsPerPeriod();
+    }
+
+    @ManagedAttribute
+    public long getPeriod() {
+        return timedSemaphore.getPeriod();
+    }
+
+    @ManagedAttribute
+    public TimeUnit getUnit() {
+        return timedSemaphore.getUnit();
+    }
+
 }
