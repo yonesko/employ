3ce1194d41bdd86c94c1d9a96a14a3cf3d9a926f
User <kuk123>
2017-08-02 21:08:21 +0300

    jmeter; performance enhance

--- doubledata/src/main/java/gleb/data/TaskRepoSimple.java
+++ doubledata/src/main/java/gleb/data/TaskRepoSimple.java
@@ -3,16 +3,18 @@ package gleb.data;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 import gleb.data.model.Task;
+import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskRepoSimple implements TaskRepo {
+    private static Logger logger = Logger.getLogger(TaskRepoSimple.class);
     private Table<String, Integer, Task> taskTable = HashBasedTable.create();
 
     public synchronized void save(String userId, Task task) {
         taskTable.put(userId, task.getId(), task);
-        System.out.println(String.format("Saved %s task for user %s", task, userId));
+        if (logger.isTraceEnabled()) logger.trace(String.format("Saved %s task for user %s", task, userId));
     }
 
     @Override
@@ -22,18 +24,18 @@ public class TaskRepoSimple implements TaskRepo {
             taskInTable.setStatusPayload(task.getStatusPayload());
             taskInTable.setStatus(task.getStatus());
 
-            System.out.println(String.format("Updated %s task for user %s", taskInTable, userId));
+            if (logger.isTraceEnabled()) logger.trace(String.format("Updated %s task for user %s", taskInTable, userId));
             return true;
         }
 
-        System.out.println(String.format("Failed to update %s task fo user %s: no such task in table", task, userId));
+        if (logger.isTraceEnabled()) logger.trace(String.format("Failed to update %s task fo user %s: no such task in table", task, userId));
         return false;
     }
 
     @Override
     public synchronized void delete(String userId, int id) {
         taskTable.remove(userId, id);
-        System.out.println(String.format("Removed %d task for user %s", id, userId));
+        if (logger.isTraceEnabled()) logger.trace(String.format("Removed %d task for user %s", id, userId));
     }
 
     @Override
